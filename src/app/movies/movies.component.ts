aa4842a7e2ef811a3321d6092e03a146b9721283
omeralitosun <omerttosu@gmail.com>
2023-07-30 17:50:48 +0300

    added movie filtering

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import movies from './data.json';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent {
 
-  filterText:string ="";
-  movies: any[]= movies
+export class MoviesComponent implements OnInit{
+  constructor(private activatedRoute:ActivatedRoute){}
+  movies: any[];
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params=>{
+      let filterText = params["filterText"];
+      this.movies = filterText?movies.filter((p:any)=> p.title.toLocaleLowerCase().indexOf(filterText?filterText.toLocaleLowerCase():"")!==-1):movies;
+    });
+  }
 }
